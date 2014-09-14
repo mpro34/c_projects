314f549dfcab0b9449abbef93c38014f7d4a77ed
mpro34 <mpro34@gmail.com>
2014-09-13 23:48:38 -0700

    Added interface for user to choose an option

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -16,28 +16,30 @@
 using namespace std; 
 
 void gameStart() {
-    srand(time(NULL));  //To generate unique cards.
-    cout << "Welcome to BlackJack!\n";
-    cout << "Here are your cards: \n";
+    int choice = 0;
+    while (choice != 3) {
+        srand(time(NULL));  //To generate unique cards.
+        cout << "Welcome to BlackJack!\n";
+        cout << "Here are your cards: \n";
+        Card c1;
+        Card c2;
+        c1.generate();
+        c2.generate();
+        cout << "\n";  //generate new line
+        c1.showCard();
+        c2.showCard();
+        cout << "\n";   //generate new line
+        cout << "Please Enter an option: (1)Hit, (2)Stay, (3)Quit...\n"; 
+        cin >> choice;
+    }
 };
 
 /*
  * 
  */
 int main(int argc, char** argv) {
- //   srand(time(NULL));  //To generate unique cards.
- //   cout << "Welcome to BlackJack!\n";
- //   cout << "Here are your cards: \n";
-    Card c1;
-    Card c2;
     gameStart();
-    c1.generate();
-    c2.generate();
-    cout << "\n";  //generate new line
-    c1.showCard();
-    c2.showCard();
-    cout << "\n";   //generate new line
-    cout << "Please Enter an option: (H)it, (S)tay, (Q)uit...\n";
+
     return 0;
 }
 
