dbcd4567356e1bd91e2d8960133acc053107482d
Chris Whiting <chris.whiting@panasonic.aero>
2017-02-23 08:09:30 -0800

    cleanup

--- hamming.c
+++ hamming.c
@@ -6,19 +6,23 @@ int hammingDistance(int x, int y);
 
 int main()
 {
-    printf("%d\n", hammingDistance(93,73));
+    //printf("%d\n", hammingDistance(93,73));
+    printf("%d\n", hammingDistance(1,4));
     return 0;
 }
 
 
 int hammingDistance(int x, int y) {
     /*First must convert x and y to binary */
-    int buffx[50], buffy[50], i=1, j=1, found_from_start=0, found_from_end=0, hamming=0;
+    int buffx[50], buffy[50], i=1, j=1, hamming=0;
     int copyx = x;
     int copyy = y;
 
+    buffx[i] = 0;
+    buffy[j] = 0;
     /* Convert x and y to binary */
     while (copyx != 0) {
+        printf("copyx: %d, mod: %d\n", copyx, (copyx % 2));
         buffx[i++] = copyx % 2;
         copyx = copyx / 2;
     }
@@ -26,36 +30,21 @@ int hammingDistance(int x, int y) {
         buffy[j++] = copyy % 2;
         copyy = copyy / 2;
     }
-
     /* Get minimum value of i or j, such that we can match lengths of copy arrays. */
     if (i >= j) {
       j = i;
     } else {
       i = j;
     }
-
+    for (int p=i; p > 0; p--) {
+      printf("x: %d   y: %d\n", buffx[p], buffy[p]);
+    }
     /* Search from end to start */
     for (int c=i-1; c > 0; c--) {
+        printf("buffx: %d  buffy: %d\n", buffx[c], buffy[c]);
         if (buffx[c] != buffy[c]) {
-          found_from_end = c;
-          break;                    //Get out as soon as a diff
+          hamming++;                    //Increment if bits are different
         }
     }
-    /* Search from start to end */
-    for (int c=0; c < i; c++) {
-        if (buffx[c] != buffy[c]) {
-          found_from_start = c;
-          break;                     //Get out as soon as a diff
-        }
-    }
-
-    printf("start: %d, end %d\n", found_from_start, found_from_end);
-    /* Calculate hamming distance */
-    if (found_from_start >= found_from_end) {
-      hamming = found_from_start - found_from_end;
-    } else {
-      hamming = found_from_end - found_from_start;
-    }
-
     return hamming;
 };
